93b2f1d7df5bb32e5b0f4d6fc28a1746edf976b1
Nicolas <nicolasfpdev@gmail.com>
2025-11-19 00:25:39 -0300

    feat: implementacao do navigator no App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TaskListScreen } from './src/screens/TaskListScreen';
+import { TaskFormScreen } from './src/screens/TaskFormScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Form: { id?: string } | undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TaskListScreen} options={{ title: 'Tarefas' }} />
+        <Stack.Screen name="Form" component={TaskFormScreen} options={{ title: 'Criar / Editar' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
